cf4d5e3cd523634d68933f9047e3cde6440609ed
Em-Arce <emjenarce@hotmail.com>
2023-01-10 16:37:27 +0800

    Added col #s to board display and logic for occupying cells from bottom up

--- game.rb
+++ game.rb
@@ -14,18 +14,15 @@ class Game
 
   def display_board
     puts ""
+    puts "  1   2   3   4   5   6   7"
+    puts ""
     puts "  #{@board[0]}  #{@board[1]}  #{@board[2]}  #{@board[3]}  #{@board[4]}  #{@board[5]}  #{@board[6]}  "
-    
     puts "  #{@board[7]}  #{@board[8]}  #{@board[9]}  #{@board[10]}  #{@board[11]}  #{@board[12]}  #{@board[13]}  "
-
     puts "  #{@board[14]}  #{@board[15]}  #{@board[16]}  #{@board[17]}  #{@board[18]}  #{@board[19]}  #{@board[20]}  "
-
     puts "  #{@board[21]}  #{@board[22]}  #{@board[23]}  #{@board[24]}  #{@board[25]}  #{@board[26]}  #{@board[27]}  "
-
     puts "  #{@board[28]}  #{@board[29]}  #{@board[30]}  #{@board[31]}  #{@board[32]}  #{@board[33]}  #{@board[34]}  "
-
     puts "  #{@board[35]}  #{@board[36]}  #{@board[37]}  #{@board[38]}  #{@board[39]}  #{@board[40]}  #{@board[41]}  "
-
+    puts ""
   end
 
   def position_occupied?(position)
@@ -36,51 +33,120 @@ class Game
     position.between?(0,41) && !position_occupied?(position)
   end
 
-  def allowed_positions()
-    array = [35,36,37,38,39,40,41]
-    array
+  
+
+  def get_pc_moves()
+    input = rand(1..7) 
+    input
   end
 
+  def valid_column?(column)
+    column.between?(1,7)
+  end
 
-  def update_allowable_position(array, position)
+  def get_position(column)
+    position = ""
     # binding.pry
-    if array.include?(position) && (35..41).include?(position)
-      array.delete(position)
-      array.append(28, 29, 30, 31, 32, 33, 34)
-    elsif array.include?(position) && (28..34).include?(position)
-      array.delete(position)
-      array.append(21, 22, 23, 24 , 25, 26, 27)
-    elsif array.include?(position) && (21..27).include?(position)
-      array.delete(position)
-      array.append(14, 15, 16, 17 , 18, 19, 20)
-    elsif array.include?(position) && (14..20).include?(position)
-      array.delete(position)
-      array.append(7, 8, 9, 10, 11, 12, 13)
-    elsif array.include?(position) && (7..13).include?(position)
-      array.delete(position)
-      array.append(0, 1, 2, 3, 4, 5, 6)
+    if column == 0
+      arr = [35, 28, 21, 14, 7, 0]
+      arr.each do |el|
+        if position_occupied?(el) == false
+          position = el.to_i
+          break
+        end
+      end  
+    elsif column == 1
+      arr = [36, 29, 22, 15, 8, 1]
+      arr.each do |el|
+        if position_occupied?(el) == false
+          position = el.to_i
+          break
+        end
+      end
+    elsif column == 2
+      arr = [37, 30, 23, 16, 9, 2]
+      arr.each do |el|
+        if position_occupied?(el) == false
+          position = el.to_i
+          break
+        end
+      end  
+    elsif column == 3
+      arr = [38, 31, 24, 17, 10, 3]
+      arr.each do |el|
+        if position_occupied?(el) == false
+          position = el.to_i
+          break
+        end
+      end
+    elsif column == 4
+      arr = [39, 32, 25, 18, 11, 4]
+      arr.each do |el|
+        if position_occupied?(el) == false
+          position = el.to_i
+          break
+        end
+      end
+    elsif column == 5
+      arr = [40, 33, 26, 19, 12, 5]
+      arr.each do |el|
+        if position_occupied?(el) == false
+          position = el.to_i
+          break
+        end
+      end
+    elsif column == 6
+      arr = [41, 34, 27, 20, 13, 6]
+      arr.each do |el|
+        if position_occupied?(el) == false
+          position = el.to_i
+          break
+        end
+      end
+    else
+      # return 
     end
-    array
+    position
   end
 
-  def turn
-    #  uncomment when isolated debug
-    @turn_count = 1
+  def turn()
+    #  uncomment when isolated debug or when play is not used
+    # @turn_count = 1
     @current_player = @turn_count.odd? ? @player1 : @player2
-    # @allowed_positions = if turn_count == 1
-    #                       allowed_positions()
-    #                   else
-    @allowed_positions = allowed_positions
-    puts "#{@current_player} choose a number between 36 - 42"
-    position = gets.chomp.to_i
-    position -= 1
-    @updated_allowable_positions = update_allowable_position(@allowed_positions, position).sort
+    puts "#{@current_player.name} choose a column from 1 to 7"
+    if @current_player.name === "Paimon"
+      column = get_pc_moves()
+    else
+      column = gets.chomp.to_i
+      until valid_column?(column)
+        print "Invalid column, enter any number from 1 to 7" 
+        column = gets.chomp.to_i
+      end
+    end
+    column -= 1
+    position = get_position(column)
     if valid_move?(position)
       @board[position] = @current_player.color
+      puts "#{@current_player.name} took cell #{position + 1}"
     else
-      puts "invalid input between #{allowable_positions}"
+      puts "invalid input, choose another column 1 to 7"
       turn
     end
-    display_board()
+    # display_board
+  end
+
+  def play
+    puts "Initialize Board"
+    display_board
+    @turn_count = 0
+    until @turn_count == 4
+      @turn_count += 1
+      turn()
+      puts "----------CURRENT BOARD STATUS----------"
+      puts "Turn Number: #{@turn_count}"
+      puts "Valid moves are white cells occupied from bottom up"
+      display_board
+    end
+    puts "GAME OVER"
   end
 end
