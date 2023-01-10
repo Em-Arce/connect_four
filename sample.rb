0896727478be656e8265940cce882ac9ca5cf500
Em-Arce <emjenarce@hotmail.com>
2023-01-10 16:38:12 +0800

    Current progress

--- sample.rb
+++ sample.rb
@@ -36,19 +36,19 @@ require_relative "game.rb"
 
 # @player1 = Player.new("Ayato", "blue")
 # @player1.display()
-@board = Array.new(42, "\u26aa")
-puts ""
-puts "  #{@board[0]}  #{@board[1]}  #{@board[2]}  #{@board[3]}  #{@board[4]}  #{@board[5]}  #{@board[6]}  "
+# @board = Array.new(42, "\u26aa")
+# puts ""
+# puts "  #{@board[0]}  #{@board[1]}  #{@board[2]}  #{@board[3]}  #{@board[4]}  #{@board[5]}  #{@board[6]}  "
 
-puts "  #{@board[7]}  #{@board[8]}  #{@board[9]}  #{@board[10]}  #{@board[11]}  #{@board[12]}  #{@board[13]}  "
+# puts "  #{@board[7]}  #{@board[8]}  #{@board[9]}  #{@board[10]}  #{@board[11]}  #{@board[12]}  #{@board[13]}  "
 
-puts "  #{@board[14]}  #{@board[15]}  #{@board[16]}  #{@board[17]}  #{@board[18]}  #{@board[19]}  #{@board[20]}  "
+# puts "  #{@board[14]}  #{@board[15]}  #{@board[16]}  #{@board[17]}  #{@board[18]}  #{@board[19]}  #{@board[20]}  "
 
-puts "  #{@board[21]}  #{@board[22]}  #{@board[23]}  #{@board[24]}  #{@board[25]}  #{@board[26]}  #{@board[27]}  "
+# puts "  #{@board[21]}  #{@board[22]}  #{@board[23]}  #{@board[24]}  #{@board[25]}  #{@board[26]}  #{@board[27]}  "
 
-puts "  #{@board[28]}  #{@board[29]}  #{@board[30]}  #{@board[31]}  #{@board[32]}  #{@board[33]}  #{@board[34]}  "
+# puts "  #{@board[28]}  #{@board[29]}  #{@board[30]}  #{@board[31]}  #{@board[32]}  #{@board[33]}  #{@board[34]}  "
 
-puts "  #{@board[35]}  #{@board[36]}  #{@board[37]}  #{@board[38]}  #{@board[39]}  #{@board[40]}  #{@board[41]}  "
+# puts "  #{@board[35]}  #{@board[36]}  #{@board[37]}  #{@board[38]}  #{@board[39]}  #{@board[40]}  #{@board[41]}  "
 
 @player1 = Player.new("Ayaka", "\u{1f534}")
 @player1.display()
@@ -56,6 +56,8 @@ puts "  #{@board[35]}  #{@board[36]}  #{@board[37]}  #{@board[38]}  #{@board[39]
 @player2.display()
 puts "#{@player1.name} will be playing against #{@player2.name}"
 game = Game.new(@player1, @player2)
-puts "Initializing board..."
-game.display_board()
-game.turn()
+# puts "Initializing board..."
+# game.display_board()
+# game.turn()
+game.play()
+# game.get_position(0)
