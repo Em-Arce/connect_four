04fc74b2fe4151a8676378c555ae4efb1cb6f0e1
Em-Arce <emjenarce@hotmail.com>
2023-01-11 15:19:48 +0800

    Some refinements

--- main.rb
+++ main.rb
@@ -6,24 +6,35 @@ def valid_input?(input)
   input == "b" || input == "r"
 end
 
+def assign_players()
+  puts "Annyeong"
+end
+
+print "MODES"
+puts "1  2 computers"
+puts "2  player1 is human, player 2 is computer"
+puts "3  2 human players"
+puts "Enter mode: "
+@mode = gets.chomp.to_i
+
 print "Enter player1 Name : "
 @name1 = gets.chomp
 print "Enter Color  "
-print "Enter b for blue or r for red"
+puts "Enter b for blue or r for red: "
 @input1 = gets.chomp
 
 until valid_input?(@input1)
-  print "Invalid color, Enter b for blue or r for red" 
+  print "Invalid color, Enter b for blue or r for red: " 
   @input1 = gets.chomp
 end
 
 def convert_input_to_ascii(input)
-  color1 = if input == "r"
+  color = if input == "r"
     "\u{1f534}" 
   elsif input == "b"
     "\u{1f535}"
   end
-  color1
+  color
 end
 
 @color1 = convert_input_to_ascii(@input1)
@@ -45,11 +56,9 @@ end
 @color2 = computer_color(@input1)
 player2 = Player.new(@name2, @color2)
 player2.display()
-puts "It's #{@name1} vs #{@name2}"
+puts "It's #{@name1} vs #{@name2} match!"
 
-game = CONNECT_FOUR.new(player1, player2)
-puts "Initialize board..."
-game.display_board()
-game.play()
+# game = CONNECT_FOUR.new(player1, player2)
+# game.play()
 
 
